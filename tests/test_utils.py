2cf60950f5377d6c8115504e446000264c0d7c8e
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2024-06-21 00:12:31 +0400

    Refactored test_utils.py to remove redundant test cases.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,12 +3,3 @@ from gmsa import utils
 
 class TestUtils(object):
 
-    def test_extract_and_format_to_header(self):
-        input_str = '"Anete Gludīte" <john.doe@example.com>'
-        expected_output = 'Anete Gludīte <john.doe@example.com>'
-        assert utils.extract_and_format_to_header(input_str) == expected_output
-
-    def test_extract_and_format_to_header_invalid_format(self):
-        input_str = 'Anete Gludīte <john.doe@example.com>'
-        with pytest.raises(ValueError):
-            utils.extract_and_format_to_header(input_str)
