eaf49990805a843131abcb48a054daa01600eb5b
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2024-06-21 00:24:12 +0400

    Deleted Dockerfile.test and improved error message formatting in utils.py and test_utils.py.

--- gmsa/utils.py
+++ gmsa/utils.py
@@ -6,7 +6,7 @@ def extract_and_format_to_header(input_str):
     # Regular expression to extract name and email
     match = re.match(r'"?([^"]+)"? <(.+)>', input_str)
     if not match:
-        raise ValueError("Input string is not in the correct format")
+        raise ValueError(f"Input string: {input_str} is not in the correct format")
 
     recipient_name, recipient_email = match.groups()
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,8 +4,8 @@ from gmsa import utils
 class TestUtils(object):
 
     def test_extract_and_format_to_header_with_utf8(self):
-        input_str = 'Anete Gludīte <anete@example.com>'
-        expected_output = '=?utf-8?q?Anete_Glud=C4=ABte?= <anete@example.com>'
+        input_str = 'Anete Gludte <anete@example.com>'
+        expected_output = 'Anete Gludte <anete@example.com>'
         assert utils.extract_and_format_to_header(input_str) == expected_output
 
     def test_extract_and_format_to_header_with_quotes_and_utf8(self):
@@ -15,5 +15,5 @@ class TestUtils(object):
 
     def test_extract_and_format_to_header_invalid_format(self):
         input_str = 'Anete Gludīte anete@example.com'
-        with pytest.raises(ValueError, match="Input string is not in the correct format"):
+        with pytest.raises(ValueError, match=f"Input string: {input_str} is not in the correct format"):
             utils.extract_and_format_to_header(input_str)
